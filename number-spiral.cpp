5ee10432e2e2f67c4208ccf1ea0a370925e21dd3
Suryansh Shukla <suryanshshukla23@lpu.in>
2026-03-20 10:05:36 +0530

    Number Spiral Problem Done

---
+++ number-spiral.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    int row, col;
+    cin >> row >> col;
+    int layer = max(row, col);
+    if (layer % 2 == 1) swap (row, col);
+    if (col == layer) cout << (layer - 1) * (layer - 1) + row << endl;
+    else cout << layer * layer - col + 1;
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+}
