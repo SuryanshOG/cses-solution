824324ab6f337318dec2bc962d9b9c4b9b3f8e4a
Suryansh Shukla <suryanshshukla23@lpu.in>
2026-03-20 11:14:45 +0530

    Solved Trailing Zeroes

---
+++ trailing-zeroes.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long n;
+    cin >> n;
+    int ans = 0;
+    for (int i = 5; i <= n; i *= 5) {
+        ans += n / i;
+    }
+    cout <<  ans << endl;
+}
