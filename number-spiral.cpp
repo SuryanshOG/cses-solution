b731e4182d2d13fc0b20c594fb160f83160b1958
Suryansh Shukla <suryanshshukla23@lpu.in>
2026-03-20 10:10:40 +0530

    Fix for endl in NUmber spiral

--- number-spiral.cpp
+++ number-spiral.cpp
@@ -5,8 +5,8 @@ void solve() {
     cin >> row >> col;
     int layer = max(row, col);
     if (layer % 2 == 1) swap (row, col);
-    if (col == layer) cout << (layer - 1) * (layer - 1) + row << endl;
-    else cout << layer * layer - col + 1;
+    if (col == layer) cout << (long long)(layer - 1) * (layer - 1) + row << endl;
+    else cout << (long long)layer * layer - col + 1 << endl;
 }
 int main() {
     int t;
